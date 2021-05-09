39fdb51b863a26b310e184a7a226c8179279a667
zhangxinfang520 <44743757+zhangxinfang520@users.noreply.github.com>
2021-05-09 21:54:36 +0800

    2021-05-09 添加了链表操作

--- linked_list/160_相交链表.py
+++ linked_list/160_相交链表.py
@@ -16,6 +16,14 @@ intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
 解释：这两个链表不相交，因此返回 null。
 
 意思 两个链表有相同的结尾
+
+答案思路 真的巧
+一种比较巧妙的方式是，分别为链表A和链表B设置指针A和指针B，然后开始遍历链表，如果遍历完当前链表，则将指针指向另外一个链表的头部继续遍历，直至两个指针相遇。
+最终两个指针分别走过的路径为：
+指针A :a+c+b
+指针B :b+c+a
+明显 a+c+b = b+c+a,因而如果两个链表相交，则指针A和指针B必定在相交结点相遇。
+
 '''
 from linked_list.leetcode import generator_link,travel
 
@@ -26,6 +34,7 @@ class ListNode:
 
 
 class Solution:
+    '''我的暴力解法 超时'''
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         if not  headA or not headB:
             return
@@ -42,14 +51,25 @@ class Solution:
                             pre_A = pre_A.next
                     if not pre_A and not pre_B:
                         return x
+                    continue
                 else:
                     pre_B = pre_B.next
             pre_B = re
             pre_A = pre_A.next
-        return
+        return x
+    '''答案解法'''
+    def getintersectionNode(self,headA: ListNode, headB: ListNode)->ListNode:
+        if not headA or not headB:
+            return None
+        nodeA = headA
+        nodeB = headB
+        while (nodeA != nodeB):
+            nodeA = nodeA.next if nodeA else headB
+            nodeB = nodeB.next if nodeB else headA
+        return nodeA
 
 listA = [4,1,8,4,5]
 listB = [5,0,1,8,4,5]
 linkA = generator_link(listA)
 linkB = generator_link(listB)
-travel(Solution().getIntersectionNode(linkA,linkB))
+travel(Solution().getintersectionNode(linkA,linkB))
