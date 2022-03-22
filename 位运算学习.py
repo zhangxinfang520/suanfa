fc2355fb20b847bd28518e8942b43b41a04fa2a4
zhangxinfang <503919940@qq.com>
2022-03-22 12:39:48 +0800

    2022-3-22

--- 位运算学习.py
+++ 位运算学习.py
@@ -4,6 +4,8 @@
 @file: 位运算学习.py
 @time: 2022/3/21 15:57
 """
+import heapq
+
 '''
 & 与运算 两位为1 才为1
 | 或运算 两位为0 才问0
@@ -17,7 +19,7 @@
 #左移
 #左移n位 代表乘以 2^n
 #右移n为 代表除以 2^n
-print(8>>2)
+# print(8>>2)
 
 #两个数 进行交换
 
@@ -30,9 +32,77 @@ def swap(a,b):
 def get_abs(a):
     return ~a +1
 
+class Income:
+    def __init__(self,cost,profit):
+        self.cost = cost
+        self.profit = profit
+
+def cmp(a,b):
+    if a.cost > b.cost:
+        return 1
+    elif a.cost < b.cost:
+        return -1
+    else:
+        if a.profit < b.profit:
+            return 1
+        else:
+            return -1
+
+class Solution:
+    def get_max(self,N,K,W,cost,profit):
+        q = []
+        while K > 0:
+            for i in range(len(cost)):
+                if cost[i] < W :
+                    #优先队列
+                    heapq.heappush(q,-profit[i])
+            if not q:
+                return W
+            else:
+                #获取最大的
+                max_=-heapq.heappop(q)
+                index = profit.index(max_)
+                cost.pop(index)
+                W += max_
+            K -= 1
+        return W
+
+
+
+
+
+
+        #
+        # # income = dict(zip(cost,profit))
+        # # result= sorted(income.items(),key=lambda x:(x[0],x[1]))
+        # # print(result)
+        # # # 先 找 W 开始 可以投资的
+        # # total = 0
+        # # #不投资
+        # # dp_i_0 = W
+        # #投资
+        # dp_i_1 = float("-inf")
+        # # for i,w in enumerate(cost):
+        # #     if w < W :
+        # #         #证明可以投资
+        # #         #不投资
+        # #         dp_i_0 = w
+        # #         #投资
+        # #         dp_i_1 = max(dp_i_1,income[i] + dp_i_1)
+
+
+
+
+
+
+
 if __name__ == '__main__':
     a = 4
-    b = 5
-    #print(swap(a,b))
-    print(get_abs(-a))
-
+    b = 9
+    N, W, K = 5,3,2
+    cost = [5,4,1,2,2,3]
+    profit = [3,5,3,2,7,7]
+    print(Solution().get_max(N, K, W, cost, profit))
+    # #print(swap(a,b))
+    # print(get_abs(-a))
+    #rint(b)
--- 剑指offer/011_0和1数组相同的子数组.py
+++ 剑指offer/011_0和1数组相同的子数组.py
@@ -16,3 +16,56 @@
 
 '''
 
+class Solution(object):
+    def findMaxLength1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        if sum(nums) == n // 2:
+            return n
+        #滑动窗口做
+
+        res = 0
+        for i in range(0,n):
+            res = max(res,self.get_len(i,nums))
+        return res
+
+    def get_len(self,i,nums):
+        n = len(nums)
+        if i == n-1:
+            return 0
+        res = 0
+        while i < n:
+            if (n-i) % 2 == 0 and sum(nums[i:n]) == (n-i)//2:
+                res = max(res,n-i)
+            n -=1
+        return res
+
+    def findMaxLength(self, nums):
+        # 0 转化为 -1 转化为 前缀和
+        # 只要 两个 sum相同 证明 这两个sum之间 0和1之间 数量相等
+        n = len(nums)
+        if n == 0 : return 0
+
+        res = 0
+        sum_, memo = 0,dict()
+        memo[0] = -1
+        for i in range(n):
+            sum_ += 1 if nums[i] == 1 else -1
+            if sum_ in memo.keys():
+                res = max(res,i-memo[sum_])
+            else:
+                memo[sum_] = i
+        return res
+
+
+if __name__ == '__main__':
+    nums = [0,1,1,1,0,0]
+    print(Solution().findMaxLength(nums))
+
+
+
